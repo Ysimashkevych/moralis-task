3a37f0808eeb22f98e0c110f730096807206134b
Yurii Simashkevych <yurii@yuriisimashkevych.com>
2024-07-18 19:39:05 +0200

    List of available for creation nodes properties

---
+++ src/ui/resources/nodes-protocol-properties.ts
@@ -0,0 +1,19 @@
+export const ProtocolsProperties = {
+    Ethereum: ["Mainnet", "Sepolia", "Holesky"],
+    Base: ["Mainnet", "Sepolia"],
+    Polygon: ["Mainnet", "Amoy"],
+    Binance: ["Mainnet", "Testnet"],
+    Arbitrum: ["Mainnet", "Sepolia"],
+    Optimism: ["Mainnet", "Sepolia"],
+    Gnosis: ["Mainnet", "Testnet"],
+    Linea: ["Mainnet", "Sepolia"],
+    Avalanche: ["Mainnet"],
+    Moonbeam: ["Mainnet", "Moonriver", "Moonbase"],
+    Blast: ["Mainnet", "Sepolia"],
+    Mantle: ["Mainnet", "Sepolia"],
+    PolygonZkEVM: ["Mainnet", "Cardona"],
+    ZkSync: ["Mainnet", "Sepolia"],
+    Zetachain: ["Mainnet", "Testnet"],
+    Fantom: ["Mainnet"],
+    OpBNB: ["Mainnet"]
+};
