76d61b8b73cabfb18a64e8997a8f17f4eb343756
Yurii Simashkevych <yurii@yuriisimashkevych.com>
2024-07-18 05:43:39 +0200

    Login page

---
+++ src/ui/pages/login.page.ts
@@ -0,0 +1,27 @@
+import { Locator, Page } from "@playwright/test";
+
+export class LoginPage {
+    readonly page: Page;
+    readonly emailInput: Locator;
+    readonly passwordInput: Locator;
+    readonly logInButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.emailInput = page.locator('#admin-login-email');
+        this.passwordInput = page.locator('#admin-login-password');
+        this.logInButton = page.locator('button', { hasText: 'Log In' });
+    }
+
+    async setEmail(email: string) {
+        await this.emailInput.fill(email);
+    };
+
+    async setPassword(password: string) {
+        await this.passwordInput.fill(password);
+    };
+
+    async clickLogin() {
+        await this.logInButton.click();
+    };
+}
