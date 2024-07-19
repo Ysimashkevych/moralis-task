11bbd1e31517af0eabafbd7353c19f5b2af71506
Yurii Simashkevych <yurii@yuriisimashkevych.com>
2024-07-19 06:12:56 +0200

    Adding site symbols (used in url) that are different for each protocol-network pari. This would later be used for verification in positive test

--- src/ui/resources/nodes-protocol-properties.ts
+++ src/ui/resources/nodes-protocol-properties.ts
@@ -17,3 +17,23 @@ export const ProtocolsProperties = {
     Fantom: ["Mainnet"],
     OpBNB: ["Mainnet"]
 };
+
+export const SiteProtocolSymbols = {
+    Ethereum: { Mainnet: "eth", Sepolia: "sepolia", Holesky: "holesky" },
+    Base: { Mainnet: "base", Sepolia: "base-sepolia" },
+    Polygon: { Mainnet: "polygon", Amoy: "amoy" },
+    Binance: { Mainnet: "bcs", Testnet: "bcs-testnet" },
+    Arbitrum: { Mainnet: "arbitrum", Sepolia: "arbitrum-sepolia" },
+    Optimism: { Mainnet: "optimism", Sepolia: "optimism-sepolia" },
+    Gnosis: { Mainnet: "gnosis", Testnet: "gnosis-testnet" },
+    Linea: { Mainnet: "linea", Sepolia: "linea-sepolia" },
+    Avalanche: { Mainnet: "avalanche" },
+    Moonbeam: { Mainnet: "mooonbeam", Moonriver: "moonriver", Moonbase: "moonbase" },
+    Blast: { Mainnet: "blast", Sepolia: "blast-sepolia" },
+    Mantle: { Mainnet: "mantle", Sepolia: "mantle-sepolia" },
+    PolygonZkEVM: { Mainnet: "polygon-zkevm", Cardona: "polygon-zkevm-cardona" },
+    ZkSync: { Mainnet: "zksync", Sepolia: "zksync-sepolia" },
+    Zetachain: { Mainnet: "zetachain", Testnet: "zetachain-testnet" },
+    Fantom: { Mainnet: "fantom" },
+    OpBNB: { Mainnet: "opbnb" }
+}
